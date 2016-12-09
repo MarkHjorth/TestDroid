2bb49d5c7d224ac78efe5d52f49618c061d9f635
Dina Sandvad <dina@Dinas-MBP.ucn.moc>
2016-12-09 09:09:58 +0100

    Forbindelse fra serversiden

---
+++ TestDroid/TestDroid/Logic/Controller/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.Telephony;
+namespace TestDroid
+{
+	public class Controller
+	{
+		private SmsManager smsManager;
+
+		public Controller()
+		{
+			
+		}
+
+		public bool SendSMS(string text = "Bush Did 9/11")
+		{
+			bool succes = false;
+			string phoneNumber = "41618934";
+
+
+			try
+			{
+				smsManager = SmsManager.Default;
+				smsManager.SendTextMessage(phoneNumber, null, text, null, null);
+			}
+			catch
+			{ 
+			}
+
+		}
+	}
+}
