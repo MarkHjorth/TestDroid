9a54da3755d46c46df70b23b333f78eaede31859
Dina Sandvad <dina@Dinas-MacBook-Pro.local>
2016-12-12 14:57:14 +0100

    smsSend sends sms-message to parameter defined phone number and with parameter defined message.

---
+++ TestDroid/TestDroid/Logic/CommandClasses/SMS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.Telephony;
+
+namespace TestDroid
+{
+	public class SMS
+	{
+		private SmsManager smsManager;
+		private static SMS instance;
+	
+		private SMS()
+		{
+			smsManager = SmsManager.Default;
+
+		}
+
+		//Calls and returns the only possible instance of SMS
+		public static SMS GetInstance()
+		{
+			if (instance == null)
+			{
+				instance = new SMS();
+			}
+			return instance;
+		}
+
+		public bool SendSMS(string text, string phoneNumber)
+		{
+			bool succes = false;
+
+			try
+			{
+				
+				smsManager.SendTextMessage(phoneNumber, null, text, null, null);
+				succes = true;
+			}
+			catch
+			{
+				succes = false;
+			}
+			return succes;
+		}
+
+	}
+}
