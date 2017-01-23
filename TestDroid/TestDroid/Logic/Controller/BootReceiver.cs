46b92d59b277d6fbcddce406d54c57ae59e54cdf
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2017-01-23 13:58:16 +0100

    Added BootReceiver class to automatically start app on phone boot

---
+++ TestDroid/TestDroid/Logic/Controller/BootReceiver.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+
+namespace TestDroid
+{
+	/// <summary>
+	/// Recieve boot to handle what happens when phone is rebooted
+	/// </summary>
+	[BroadcastReceiver]
+	[IntentFilter(new[] { Android.Content.Intent.ActionBootCompleted },
+		Categories = new[] { Android.Content.Intent.CategoryDefault })]
+	public class ReceiveBoot : BroadcastReceiver
+	{
+		/// <summary>
+		/// Overrides OnReceive for the boot intent - to start the app when phone boots.
+		/// </summary>
+		/// <param name="context">Context.</param>
+		/// <param name="intent">Intent.</param>
+		public override void OnReceive(Context context, Intent intent)
+		{
+			Toast.MakeText(context, "Starting TestDroid", ToastLength.Long).Show();
+
+			if ((intent.Action != null) && (intent.Action == Android.Content.Intent.ActionBootCompleted))
+			{ 	
+				Android.Content.Intent start = new Android.Content.Intent(context, typeof(MainActivity));
+
+				start.AddFlags(ActivityFlags.NewTask);
+				context.ApplicationContext.StartActivity(start);
+			}
+		}
+	}
+}
